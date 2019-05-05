bbd166a457a788323e1321a6db8edea22f2e0b21
Lera Green <zelnayavaleriya@gmail.com>
2019-05-05 21:10:04 +0300

    добавляет компоненту для списка отелей

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import HotelsPage from '../hotels-page/hotels-page.jsx';
 
-const App = () => {
-  return <HotelsPage />;
+const App = (props) => {
+  const hotels = props.hotels;
+
+  return <HotelsPage 
+    hotels={hotels}
+  />;
 };
 
 export default App;
