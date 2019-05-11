96455feece52a1d65eb94e39b774a26311c5d597
Lera Green <zelnayavaleriya@gmail.com>
2019-05-11 23:21:14 +0300

    делает компонент

--- src/components/hotel-card/hotel-card.jsx
+++ src/components/hotel-card/hotel-card.jsx
@@ -1,53 +1,51 @@
-import React, {Component} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-// const hotel = props.hotel;
-// const onClick = props.onClick;
+const HotelCard = (props) => {
+  const hotel = props.hotel;
+  const onClick = props.onClick;
+  const index = props.index;
 
-class HotelCard extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return <article className="cities__place-card place-card">
-      <div className="place-card__mark">
-        <span></span>
-      </div>
-      <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src={hotel.photo} width="260" height="200" alt="Place image"></img>
-        </a>
-      </div>
-      <div className="place-card__info">
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{hotel.price}</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
-          </div>
-          <button className="place-card__bookmark-button button" type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"></use>
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
+  return <article className="cities__place-card place-card">
+    <div className="place-card__mark">
+      <span></span>
+    </div>
+    <div className="cities__image-wrapper place-card__image-wrapper">
+      <a href="#">
+        <img className="place-card__image" src={hotel.photo} width="260" height="200" alt="Place image"></img>
+      </a>
+    </div>
+    <div className="place-card__info">
+      <div className="place-card__price-wrapper">
+        <div className="place-card__price">
+          <b className="place-card__price-value">&euro;{hotel.price}</b>
+          <span className="place-card__price-text">&#47;&nbsp;night</span>
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-  
-            <span style={{width: `${hotel.rating}%`}}></span>
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
-        <h2 className="place-card__name" onClick={onClick}>
-          <a href="#">{hotel.description}</a>
-        </h2>
-        <p className="place-card__type">{hotel.type}</p>
+        <button className="place-card__bookmark-button button" type="button">
+          <svg className="place-card__bookmark-icon" width="18" height="19">
+            <use xlinkHref="#icon-bookmark"></use>
+          </svg>
+          <span className="visually-hidden">To bookmarks</span>
+        </button>
       </div>
-    </article>;
-  }
-}
-
+      <div className="place-card__rating rating">
+        <div className="place-card__stars rating__stars">
+          <span style={{width: `${hotel.rating}%`}}></span>
+          <span className="visually-hidden">Rating</span>
+        </div>
+      </div>
+      <h2 className="place-card__name" onClick={(evt) => {
+        if (onClick && index !== undefined) {
+          evt.preventDefault();
+          onClick(index);
+        }
+      }}>
+        <a href="#">{hotel.description}</a>
+      </h2>
+      <p className="place-card__type">{hotel.type}</p>
+    </div>
+  </article>;
+};
 
 HotelCard.propTypes = {
   hotel: PropTypes.shape({
@@ -57,7 +55,8 @@ HotelCard.propTypes = {
     rating: PropTypes.number.isRequired,
     photo: PropTypes.string.isRequired
   }).isRequired,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  index: PropTypes.number
 };
 
 export default HotelCard;
