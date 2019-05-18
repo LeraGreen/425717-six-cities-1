f3d5d3c7c36fbf67e359e157acd6132f901b42c5
Lera Green <zelnayavaleriya@gmail.com>
2019-05-18 15:58:56 +0300

    меняет пин

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -25,11 +25,21 @@ class Map extends PureComponent {
     });
     map.setView(city, zoom);
 
+    const icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
+
     leaflet
     .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, { 
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
     })
     .addTo(map);
+
+    const offerCords = [52.3709553943508, 4.89309666406198];
+    leaflet
+      .marker(offerCords, {icon})
+      .addTo(map);
   }
 }
 
