ed98d83feb4ae390329e7887b2bb13328ec344eb
Lera Green <zelnayavaleriya@gmail.com>
2019-06-01 17:26:57 +0300

    добавляет города к отелям

--- src/mocks/hotels.js
+++ src/mocks/hotels.js
@@ -1,5 +1,6 @@
 const hotels = [
   {
+    city: `Paris`,
     description: `Beautiful & luxurious apartment at great location`,
     price: 120,
     type: `Apartment`,
@@ -8,6 +9,7 @@ const hotels = [
     coordinates: [52.3909553943508, 4.85309666406198]
   },
   {
+    city: `Paris`,
     description: `Wood and stone place`,
     price: 200,
     type: `Private Room`,
@@ -16,6 +18,7 @@ const hotels = [
     coordinates: [52.369553943508, 4.85309666406198]
   },
   {
+    city: `Cologne`,
     description: `Canal View Prinsengracht`,
     price: 80,
     type: `Private Room`,
@@ -24,6 +27,34 @@ const hotels = [
     coordinates: [52.3909553943508, 4.929309666406198]
   },
   {
+    city: `Cologne`,
+    description: `Nice, cozy, warm big bed apartment`,
+    price: 90,
+    type: `Apartment`,
+    rating: 80,
+    photo: `img/apartment-02.jpg`,
+    coordinates: [52.3809553943508, 4.939309666406198]
+  },
+  {
+    city: `Amsterdam`,
+    description: `Nice, cozy, warm big bed apartment`,
+    price: 90,
+    type: `Apartment`,
+    rating: 80,
+    photo: `img/apartment-02.jpg`,
+    coordinates: [52.3809553943508, 4.939309666406198]
+  },
+  {
+    city: `Brussels`,
+    description: `Nice, cozy, warm big bed apartment`,
+    price: 90,
+    type: `Apartment`,
+    rating: 80,
+    photo: `img/apartment-02.jpg`,
+    coordinates: [52.3809553943508, 4.939309666406198]
+  },
+  {
+    city: `Dusseldorf`,
     description: `Nice, cozy, warm big bed apartment`,
     price: 90,
     type: `Apartment`,
