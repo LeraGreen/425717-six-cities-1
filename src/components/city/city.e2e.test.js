933f9609891580f4708500b79b80afe922a81933
Lera Green <zelnayavaleriya@gmail.com>
2019-06-09 00:06:03 +0300

    добавляет тесты на city

---
+++ src/components/city/city.e2e.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import City from "./city.jsx";
+
+configure({adapter: new Adapter()});
+
+const mock = {
+  city: `Chelyaba`,
+  isActive: false,
+  onCityClick: jest.fn(),
+  onCardClick: jest.fn()
+};
+
+it(`Click on city title calls callback`, () => {
+  const {city, onCityClick, onCardClick, isActive} = mock;
+  const clickHandler = jest.fn();
+
+  const cityTab = mount(<City
+    city={city}
+    isActive={isActive}
+    onCityClick={clickHandler}
+    onCardClick = {onCardClick}
+  />);
+
+
+  const tab = cityTab.find(`.locations__item-link`);
+  tab.simulate(`click`);
+  expect(clickHandler).toHaveBeenCalledTimes(1);
+});
---
+++ src/components/city/city.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import City from "./city.jsx";
+
+const mock = {
+  city: `Chelyaba`,
+  isActive: true,
+  onCityClick: jest.fn(),
+  onCardClick: jest.fn()
+};
+
+it(`App renders correctly`, () => {
+  const {city, onCityClick, onCardClick, isActive} = mock;
+  const tree = renderer
+    .create(<City 
+      city={city}
+      isActive={isActive}
+      onCityClick={onCityClick}
+      onCardClick={onCardClick}
+      />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
